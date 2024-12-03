3d881c725df7a473be6c0541c81d0a7bb9474ef9
Talha Khurram <tk.shiekh456@gmail.com>
2024-12-03 03:12:10 -0800

    Updated Portfolio

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
                 
                 <Box mt={3} textAlign={matches ? "center" : ""} >
                   <CustomButton
-                    href="/Talha_Frontend_Developer_Resume3.pdf"
+                    href="/Talha_Frontend_Developer_Resume.pdf"
                     onClick={() => {}}
                     children={
                       <Typography
